a3ec634be9c4a1f0373f12987f890ac2db24ad68
BLeal-dev <brunonunesleal@outlook.com>
2026-03-05 21:33:51 +0000

    Adiciona um vídeo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home(){
-    return<h1>Te amo muito, obrigado por me apoiar!</h1>
+    return<h1>Te amo muito, obrigado por me apoiar!</h1> 
+    return<p>https://www.youtube.com/watch?v=LjhCEhWiKXk</p>
 }
 
 export default Home; 
